65f6b0aafe514690d0d99d4acfa68f2ba947ae47
Sujeeth Jinesh <sujeethjinesh@gmail.com>
2017-04-01 09:14:44 +0000

    Removed unused files

--- json_prac.py
+++
@@ -1,9 +0,0 @@
-import json
-with open('data.json', 'r') as infile:
-    data = json.load(infile)
-
-print(type(data["outputs"][0]["data"]["concepts"]))
-
-for concept in data["outputs"][0]["data"]["concepts"]:
-    name = concept["name"]
-    print(name)
